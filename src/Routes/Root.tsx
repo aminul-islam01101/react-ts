c0d2f25459ee201964b8229206b1ea2e01477622
aminulislam01101 <aminul.islam01101@gmail.com>
2023-07-12 18:11:52 +0600

    husky test

--- src/Routes/Root.tsx
+++ src/Routes/Root.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable react/jsx-no-constructed-context-values */
-/* eslint-disable no-nested-ternary */
 import { Outlet } from 'react-router-dom';
 import Footer from '../Components/Footer';
 import Navbar from '../Components/Navbar';
 
 const Root = () => {
-    console.log('Root');
+    console.log('Root hustky test');
 
     return (
         <div className=" flex flex-col justify-between min-h-screen">
